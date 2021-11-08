20938a3c6a57d0a14b9b76a31321860da0be71cc
Mariperu <maritzarg2020@gmail.com>
2021-11-07 19:43:11 -0500

    Bootstrap pract

--- src/components/EpisodesMaSort.jsx
+++ src/components/EpisodesMaSort.jsx
@@ -1,57 +1,31 @@
 import React, { useState } from "react";
 import { EpisodesMaEp } from "./EpisodesMaEp.jsx";
 
+//Esta es mi función para reutilizar `sort`
+// const sort_lists = (key, list, select) =>
+//   (select = "az"
+//     ? [...list].sort((b, a) => (a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0))
+//     : [...list].sort((a, b) =>
+//         a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0
+//       ));
+
 const EpisodesMaSort = ({ episodes }) => {
   const [sortEpis, setSortEpis] = useState(null);
-
-  //console.log(sortedField);
-  //let sortedEpisodes = [...episodes];
-  //   const orderName = (select, episodes) => {
-  //     if (select === "az") {
-  //       episodes.sort((a, b) => (a[sortEpis] < b[sortEpis] ? -1 : 1));
-  //     } else {
-  //       episodes.sort((a, b) => (a[sortEpis] > b[sortEpis] ? -1 : 1));
-  //     }
-  //   };
-
-  //   const orderName = (select, episodes) => {
-  //     if (select === "az") {
-  //       episodes.sort((a, b) => (a.name < b.name ? -1 : 1));
-  //     } else {
-  //       episodes.sort((a, b) => (a.name > b.name ? -1 : 1));
-  //     }
-  //   };
+  console.log(episodes);
 
   episodes.sort((a, b) => (a[sortEpis] < b[sortEpis] ? -1 : 1));
 
   return (
     <>
-      <section>
-        <button type="button" onClick={() => setSortEpis("name")}>
-          Sort AZ
-        </button>
+      <button
+        className="btn btn-warning"
+        type="button"
+        onClick={() => setSortEpis("name")}
+      >
+        Sort AZ
+      </button>
 
-        {/* <button type="button" onClick={() => orderName("az", episodes)}>
-          Sort by AZ
-        </button>
-        <button type="button" onClick={() => orderName("za", episodes)}>
-          Sort by ZA
-        </button> */}
-
-        {/* <select id="orderByNumber">
-          <option value="" href="" selected disabled>
-            Sort by:
-          </option>
-          <option value="az" href="">
-            A-Z
-          </option>
-          <option value="za" href="">
-            Z-A
-          </option>
-        </select> */}
-
-        <EpisodesMaEp episodes={episodes} />
-      </section>
+      <EpisodesMaEp episodes={episodes} />
     </>
   );
 };
